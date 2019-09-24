d8b6ca605905aeea756fcae1dbbaa305030445c5
Alexander <tech-cobber@yandex.ru>
2019-09-25 00:20:38 +0300

    minor improvements

--- bot/main.py
+++ bot/main.py
@@ -1,8 +1,7 @@
 from utils import read_config
 from aiogram import Bot, Dispatcher, executor, types
-from io import BytesIO
+from datetime import datetime
 import requests
-import asyncio
 
 config = read_config(f'../config.yml')
 token  = config['token']
@@ -10,12 +9,12 @@ bot    = Bot(token=token)
 dp     = Dispatcher(bot)
 
 @dp.message_handler(content_types=['photo']) 
-async def test(message: types.Message):
+async def image(message: types.Message):
     file = await bot.get_file(message.photo[len(message.photo)-1]['file_id'])
-    r = requests.post("http://127.0.0.1:8088/image", json={'name': 'image', 
-                                                           'path': f'{file.file_path}'
-                                                           }) 
-    await message.reply(r.status_code)
+    resp = requests.post("http://127.0.0.1:8088/image", json={'name': f'{datetime.now()}', 
+                                                              'path': f'{file.file_path}'
+                                                             }) 
+    await message.reply(resp.status_code)
 
 async def shutdown(dispatcher: Dispatcher):
     await dispatcher.storage.close()
